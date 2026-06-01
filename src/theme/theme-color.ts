f71bdfde54b5d44f3041c69beab705d98f96f989
Kleyton Ferreira <kleytondev40@gmail.com>
2026-06-01 13:53:00 -0300

    feat: add Colors backgroundIcon

--- src/theme/theme-color.ts
+++ src/theme/theme-color.ts
@@ -1,14 +1,15 @@
 const Colors = {
     text: {
         white: "#ffffff",
-        black: "#000000",
+        black: "#1D1D1F",
         blue: "#134386",
         blueSmooth: "#44B8C9",
         buttonwaths: "#27AE60",
 
     },
     background: {
-        backgroundBlue: "#134386"
+        backgroundBlue: "#134386",
+        backgroundIcon: "#44B8C9"
     },
 
     input: {
