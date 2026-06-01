edd42085e3b12b5d36f99bf6f5b9b8eda2a0711a
Kleyton Ferreira <kleytondev40@gmail.com>
2026-06-01 16:45:05 -0300

    feat: add banner components node home components

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,9 +1,14 @@
+// COMPONENTS
 import Header from '../../components/header/header-components'
+import Banner from '../../components/banner/banner-components'
 
 const HomePage = () => {
   return (
     <>
       <Header />
+      <section>
+        <Banner />
+      </section>
     </>
   )
 }
