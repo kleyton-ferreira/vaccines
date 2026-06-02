12f06512f66ca51d2c7c6325f89dd49898c976b1
Kleyton Ferreira <kleytondev40@gmail.com>
2026-06-02 18:56:46 -0300

    feat: add icons and script img

--- src/scripts/img-script.ts
+++ src/scripts/img-script.ts
@@ -1,9 +1,17 @@
 import Logo from '../img/logo.png'
 import Banner from "../img/banner.png"
+import IconOne from "../img/icon-1.png"
+import IconTwo from "../img/icon-2.png"
+import IconThree from "../img/icon-3.png"
+import IconFour from "../img/icon-4.png"
+import IconFive from "../img/icon-5.png"
 
 export const Images = {
   logo: Logo,
-  banner: Banner
+  banner: Banner,
+  iconOne: IconOne,
+  iconTwo: IconTwo,
+  iconThree: IconThree,
+  iconFour: IconFour,
+  iconFive: IconFive
 }
-
-
