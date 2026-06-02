8b204f3dab43f35c304fea9712e5e86dc4e0a5ef
Kleyton Ferreira <kleytondev40@gmail.com>
2026-06-02 18:59:38 -0300

    feat: add script node ResourcerTitle

--- src/scripts/text-script.ts
+++ src/scripts/text-script.ts
@@ -4,3 +4,11 @@ export const TextBanner = [
     { id: 3, title: "impulsione seu negócio" },
     { id: 4, title: "com inteligência." },
 ]
+
+export const ResourcerTitle = [
+    { id: 1, title: "Acesse uma variedade de" },
+    { id: 2, title: "relatórios avançados para" },
+    { id: 3, title: "monitorar o desempenho da sua" },
+    { id: 4, title: "clínica e identificar oportunidades" },
+    { id: 5, title: "de melhoria com facilidade." },
+]
